23aee20dfe6df27f602c5bee327df9f23cbbe684
marcodepaula <marcodepaula@gmail.com>
2019-10-04 11:08:41 -0300

    CRUD Scaffolding

---
+++ Data/SalesWebMvc3Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMvc3.Models
+{
+    public class SalesWebMvc3Context : DbContext
+    {
+        public SalesWebMvc3Context (DbContextOptions<SalesWebMvc3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvc3.Models.Department> Department { get; set; }
+    }
+}
